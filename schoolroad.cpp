bec3ddb3ff23d4f0a9342c5bb9920a282590176f
tadano_hage <92782614+hage-inkyat@users.noreply.github.com>
2022-02-14 17:10:37 +0900

    Add files via upload

--- schoolroad.cpp
+++ schoolroad.cpp
@@ -1,39 +1,29 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-#define rep(i,n) for(int i=0;i<(n);i++)
-
-int w, h, n; 
-int dp[20][20];// x y
-bool memo[20][20];// x y
-int main(void){
-    while(1){
-        rep(i, 20)rep(j, 20){//初期化
-            dp[i][j] = 0;
-            memo[i][j] = false;
-        }
-
-        cin >> w >> h;
-        if(w == 0 && h == 0) return 0;
+int main(){
+    int a,b;
+    while( cin >> a >> b , a || b ){
+        int n, dp[20][20] = {0};
+        dp[1][1] = 1;
         cin >> n;
-        rep(i, n){
-            int x, y; cin >> x >> y;
-            x--; y--;
-            memo[x][y] = true;//工事
+        for(int i=0 ; i < n ; i++ ){
+            int x,y;
+            cin >> x >> y;
+            dp[y][x] = -1; //交差点(x,y)までの経路の数が求まっていないので-1で初期化する
         }
-        dp[0][0] = 1;// x = 0, y = 0にたどり着くまでの道順の通り数
-        for (int x = 0; x < w; ++x){
-            for (int y = 0; y < h; ++y){
-                if(memo[x][y]) continue;
-                //もらうdp
-                if(0 <= x - 1 && x - 1 < w){
-                    dp[x][y] += dp[x - 1][y];
-                }
-                if(0 <= h - 1 && h - 1 < h){
-                    dp[x][y] += dp[x][y - 1];
+        for(int y=1 ; y <= b ; y++ ){
+            for(int x=1 ; x <= a ; x++ ){
+                if( dp[y][x] != -1 ){
+                    if( y != 1 && dp[y-1][x] != -1 ){
+                        dp[y][x] += dp[y-1][x];
+                    }
+                    if( x != 1 && dp[y][x-1] != -1 ){
+                        dp[y][x] += dp[y][x-1];
+                    }
                 }
             }
-        }
-        cout << dp[w - 1][h - 1] << endl;
+        }// (a,b)までの経路の数を出力
+        cout << dp[b][a] << endl;
     }
+    return(0);
 }
